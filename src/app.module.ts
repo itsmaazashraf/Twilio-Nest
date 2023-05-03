fc79105d4a75e06ad5cbff0c681db5dc7a632d42
Maaz <maaz.ashraf@turingtechnologies.org>
2023-05-03 16:08:57 +0500

    Initial commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,10 @@
+import { join } from 'path'
 import { MongooseModule } from '@nestjs/mongoose';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AudiocallModule } from './modules/audiocall/audiocall.module';
@@ -8,6 +12,11 @@ import { AudiocallModule } from './modules/audiocall/audiocall.module';
 @Module({
   imports: [
     ConfigModule.forRoot(),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      sortSchema: true,
+    }),
     MongooseModule.forRoot(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
